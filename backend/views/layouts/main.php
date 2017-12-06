026505ed8f39faf12c1456e7a934051eb5bf7819
Eumaser <eumerjoseph.ramos@yahoo.com>
2017-12-06 16:47:06 +0800

    removed footer

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -134,15 +134,17 @@ AppAsset::register($this);
 
 </section>
 
+<!---
 <footer class="site-footer">
          <div class="text-center">
              2014 - Alvarez.is
-             <!---
+
              <a href="index.html#" class="go-top">
                  <i class="fa fa-angle-up"></i>
-             </a>--->
+             </a>
          </div>
-     </footer>
+</footer>
+--->
 
 
 
